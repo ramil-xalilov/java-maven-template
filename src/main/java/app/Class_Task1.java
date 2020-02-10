5297034035dce53ae02321107e5631c5d3139106
xelilovramil2@gamil.com <xelilovramil2>
2020-02-10 19:07:18 +0400

    Calculator is done.

---
+++ src/main/java/app/Class_Task1.java
@@ -0,0 +1,16 @@
+package app;
+import java.util.Scanner;
+public class Class_Task1 {
+    public static void main(String[] args){
+        float a;
+        float b;
+        Scanner c = new Scanner(System.in);
+        System.out.println("Enter the numbers: \n");
+        a = c.nextFloat();
+        b = c.nextFloat();
+        System.out.println("Sum of to numbers:");
+        System.out.println(a + b);
+
+
+    }
+}
